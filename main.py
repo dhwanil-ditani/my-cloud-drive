a429f6d8ff6a18c4fe8f0cb97d8cc0d729d4ab23
Dhwanil Ditani <ditanidhwanil@gmail.com>
2024-10-13 13:20:07 +0530

    Cloud Drive base implementation

---
+++ main.py
@@ -0,0 +1,81 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Response, UploadFile
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from starlette.status import HTTP_200_OK, HTTP_404_NOT_FOUND
+
+sqlite_file_name = "db.sqlite3"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+    SQLModel.metadata.drop_all(engine)
+
+
+class FileBase(SQLModel):
+    name: str
+    content_type: str
+    size: int
+
+
+class File(FileBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    data: bytes
+
+
+class FileResponse(FileBase):
+    id: int
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/files/", response_model=list[FileResponse])
+def list_files(session: SessionDep):
+    stmt = select(File)
+    return session.exec(stmt).all()
+
+
+@app.post("/files/upload", response_model=FileResponse)
+def upload_file(file: UploadFile, session: SessionDep):
+    file = File(
+        name=file.filename,
+        content_type=file.content_type,
+        size=file.size,
+        data=file.file.read(),
+    )
+    session.add(file)
+    session.commit()
+    session.refresh(file)
+    return file
+
+
+@app.get("/files/{file_id}", response_model=FileResponse)
+def get_file(file_id: int, session: SessionDep):
+    file = session.get(File, file_id)
+    if not file:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="File not found.")
+    return file
+
+
+@app.get("/files/{file_id}/download")
+def download_file(file_id: int, session: SessionDep):
+    file = session.get(File, file_id)
+    if not file:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="File not found.")
+    print(file.name)
+    return Response(
+        content=file.data, media_type=file.content_type, status_code=HTTP_200_OK
+    )
